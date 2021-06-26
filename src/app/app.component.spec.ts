062e33e921ebe7f97c94fb7fd0318765cbceff8d
Yash <yaswanth.iddibani@gmail.com>
2021-06-26 17:09:07 +0100

    Assignment 07

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -5,12 +5,8 @@ import { AppComponent } from './app.component';
 describe('AppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [
-        RouterTestingModule
-      ],
-      declarations: [
-        AppComponent
-      ],
+      imports: [RouterTestingModule],
+      declarations: [AppComponent],
     }).compileComponents();
   });
 
@@ -20,16 +16,33 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'AngularAssignment01'`, () => {
+  it(`should have as title Demonstration Test Cases`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('AngularAssignment01');
+    expect(app.title).toContain('Demonstration Test Cases');
   });
 
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('AngularAssignment01 app is running!');
+  it('should load initial 3 issues', () => {
+    const comp = new AppComponent();
+    expect(comp.issues.length).toBe(3);
   });
+
+  it('should have 4 issues after adding an issue', () => {
+    const comp = new AppComponent();
+    comp.issueDescription = 'Issue Description 4';
+    comp.issueSeverity = 'Critical';
+    comp.issueStatus = 'Open';
+    comp.onFormSubmit();
+    expect(comp.issues.length).toBe(4);
+  });
+
+  // it('should render table', () => {
+  //   const fixture = TestBed.createComponent(AppComponent);
+  //   fixture.detectChanges();
+  //   const compiled = fixture.nativeElement;
+  //   console.log(compiled.querySelector('.container .row table tr'));
+  //   expect(compiled.querySelector('.container .row table tr').length).toEqual(
+  //     4
+  //   );
+  // });
 });
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,6 +7,7 @@ import { Issue } from './issue.modal';
   styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
+  title: string = 'Demonstration Test Cases';
   issueDescription: string;
   issueSeverity: string;
   issueStatus: string;
