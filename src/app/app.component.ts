7f4f20db766045cbec661026c89031484813aa48
Yash <yaswanth.iddibani@gmail.com>
2021-06-26 12:05:06 +0100

    Project Setup

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,26 @@
 import { Component } from '@angular/core';
+import { Issue } from './issue.modal';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'AngularAssignment07';
+  issueDescription: string;
+  issueSeverity: string;
+  issueStatus: string;
+  severityData: string[] = ['Critical', 'Major', 'Minor'];
+  statusData: string[] = ['Open', 'In Progress', 'Closed'];
+  issues: Issue[] = [
+    new Issue('Issue One', 'Minor', 'Open'),
+    new Issue('Issue Two', 'Major', 'In Progress'),
+    new Issue('Issue Three', 'Critical', 'Closed'),
+  ];
+
+  onFormSubmit() {
+    this.issues.push(
+      new Issue(this.issueDescription, this.issueSeverity, this.issueStatus)
+    );
+  }
 }
---
+++ src/app/issue.modal.ts
@@ -0,0 +1,7 @@
+export class Issue {
+  constructor(
+    public issueDescription: string,
+    public issueSeverity: string,
+    public issueStatus: string
+  ) {}
+}
